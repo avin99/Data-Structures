2c7d74fe4eacb42fa31e7bd3151542432a3135e4
avin99 <54173835+avin99@users.noreply.github.com>
2023-08-19 18:21:29 +0530

    Time: 3 ms (58.32%), Space: 12.5 MB (9.48%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1+str2 != str2+str1)return "";
-        return str1.substr(0,__gcd(str1.size(),str2.size()));
+        string maxDivisor = "";
+        for(int i = min (str1.size(), str2.size()); i >= 1; i--){
+            string base = str1.substr(0,i);
+            if( str1.size() % i > 0 || str2.size() % i >0)continue;
+            if(finalString(base, str1.size()/i) == str1 && finalString(base, str2.size()/i) == str2){
+                maxDivisor = base;
+                break;
+            }
+        }
+        return maxDivisor;
+    }
+    string finalString(string base, int repetition){
+        string output = "";
+        for(int i = 0 ; i < repetition ; i++){
+            output += base;
+        }
+        return output;
     }
 };
