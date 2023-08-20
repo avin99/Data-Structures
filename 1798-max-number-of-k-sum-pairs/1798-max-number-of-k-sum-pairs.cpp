ae22dfa90ed980c0f2fb2a6849f80e665686bebe
avin99 <54173835+avin99@users.noreply.github.com>
2023-08-21 00:42:02 +0530

    Time: 209 ms (15.67%), Space: 68.1 MB (24.07%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int i = 0 , j = nums.size() - 1;
+        map<int , int> mp;
+        for(auto ele: nums){
+            mp[ele]++;
+        }
         int cnt = 0;
-        while ( i < j){
-            if(nums[i] + nums [j] == k){
-                cnt++;i++;j--;
-            }else if(nums[i] + nums[j] > k){
-                j--;
-            }else{
-                i++;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(mp.find(k-nums[i]) != mp.end()){
+                if(k - nums[i] == nums[i]){
+                    if(mp[nums[i]] < 2)continue;
+                    mp[nums[i]]-=2;
+                    if(mp[nums[i]] <= 0)mp.erase(nums[i]);
+                    cnt++;
+                }else{
+                    if(mp[nums[i]] < 1 or mp[k - nums[i]] < 1)continue;
+                    mp[k-nums[i]]--;
+                    mp[nums[i]]--;
+                    if(mp[k - nums[i]] <= 0)mp.erase(k - nums[i]);
+                    if(mp[nums[i]] <= 0)mp.erase(nums[i]);
+                    cnt++;
+                }
             }
         }
         return cnt;
