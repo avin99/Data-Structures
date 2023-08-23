ba9da3b2ddc9aba6e91150cb15389aa5aea6d7fc
avin99 <54173835+avin99@users.noreply.github.com>
2023-08-23 20:06:32 +0530

    Time: 1634 ms (5.02%), Space: 11.9 MB (99.65%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -8,7 +8,7 @@ public:
             for(int start_j = 0 ; start_j < m ; start_j++){
                 if(matrix[start_i][start_j] != '1')continue;
                 int minWidth = INT_MAX;
-                for(int end_i = start_i ; end_i < n ; end_i++){
+                for(int end_i = start_i ; end_i < n && matrix[end_i][start_j] == '1' ; end_i++){
                     int width = 0;
                     for(int end_j = start_j ; end_j < m && matrix[end_i][end_j] == '1' ; end_j++){
                         width++;
