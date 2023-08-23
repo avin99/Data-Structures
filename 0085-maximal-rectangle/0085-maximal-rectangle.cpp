9c4a6bb01c1dec755ddc0ce5de238fddbd50ce02
avin99 <54173835+avin99@users.noreply.github.com>
2023-08-23 20:03:23 +0530

    Time: 1877 ms (5.02%), Space: 12 MB (97.45%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int ans = 0;
+        for(int start_i = 0; start_i < n ; start_i++){
+            for(int start_j = 0 ; start_j < m ; start_j++){
+                if(matrix[start_i][start_j] != '1')continue;
+                int minWidth = INT_MAX;
+                for(int end_i = start_i ; end_i < n ; end_i++){
+                    int width = 0;
+                    for(int end_j = start_j ; end_j < m && matrix[end_i][end_j] == '1' ; end_j++){
+                        width++;
+                    }
+                    minWidth = min(minWidth, width);
+                    ans = max(ans, (end_i - start_i + 1) * minWidth);
+                }
+            }
+        }
+        return ans;
+    }
+};
