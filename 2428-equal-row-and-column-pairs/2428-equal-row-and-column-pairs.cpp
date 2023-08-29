2350de9e9a33fa0ab3a773a9706d7646a7f303ff
avin99 <54173835+avin99@users.noreply.github.com>
2023-08-30 00:06:52 +0530

    Time: 316 ms (18.22%), Space: 23.5 MB (88.42%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        vector<string>rows;
         int n = grid.size();
         int m = grid[0].size();
         int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
-            string row = "";
-            for(int j = 0 ; j < m ; j++){
-                row += to_string(grid[i][j]) + '#';
+        for(int r = 0 ; r < n ; r++){
+            for(int c = 0 ; c < m ; c++){
+                bool match = true;
+                for(int i = 0 ; i < n ; i++){
+                    if(grid[r][i] != grid[i][c]){
+                        match = false;break;
+                    }
+                }
+                if(match)cnt++;
             }
-            rows.push_back(row);
-        }
-        for(int i = 0 ; i < n ; i++){
-            cout<<rows[i]<<" ";
-        }
-        cout<<endl;
-        for(int i = 0 ; i < m ; i++){
-            string row = "";
-            for(int j = 0 ; j < n ; j++){
-                row += to_string(grid[j][i]) + '#';
-            }
-            cout<<row<< " ";
-            cnt += count(rows.begin(), rows.end(), row);
-                
         }
         return cnt;
     }
