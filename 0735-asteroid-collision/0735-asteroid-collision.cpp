008327e894d97f7172b1c9c4c79746bb9d91417b
avin99 <54173835+avin99@users.noreply.github.com>
2023-08-31 00:59:33 +0530

    Time: 11 ms (86.61%), Space: 17.7 MB (50.77%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -5,27 +5,20 @@ public:
         stack<int>intStack;
         vector<int>answer;
         for(int i = 0 ; i < n ; i++){
-            if(intStack.empty()){
+            if(intStack.empty() || asteroids[i] > 0){
                 intStack.push(asteroids[i]);
-            }else if(intStack.top() > 0 && asteroids[i] < 0){
-                if(abs(intStack.top()) == abs(asteroids[i])){
-                    intStack.pop();
-                }else if(abs(asteroids[i]) < intStack.top()){
-                    //do nothing
-                }else{
-                    while(!intStack.empty() && intStack.top() > 0 && abs(asteroids[i]) > intStack.top()){
-                        intStack.pop();
-                    }
-                    if(!intStack.empty() && intStack.top() > 0 && intStack.top() == abs(asteroids[i])){
-                        intStack.pop();
-                    }else if(intStack.empty() || intStack.top() < 0){
-                        intStack.push(asteroids[i]);
-                    }
-                }
             }else{
-                intStack.push(asteroids[i]);
+                while(!intStack.empty() && intStack.top() > 0 && abs(asteroids[i]) > intStack.top()){
+                    intStack.pop();
+                }
+                if(!intStack.empty() && intStack.top() > 0 && intStack.top() == abs(asteroids[i])){
+                    intStack.pop();
+                }else if(intStack.empty() || intStack.top() < 0){
+                    intStack.push(asteroids[i]);
+                }
             }
         }
+    
         while(!intStack.empty()){
             answer.push_back(intStack.top());
             intStack.pop();
